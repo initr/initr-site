751df81fa578f6fa612bad262f327017118e0160
Ryan Tablada <ryan.tablada@gmail.com>
2014-03-13 22:04:08 -0500

    Initr

--- public/index.php
+++ public/index.php
@@ -58,6 +58,15 @@
 		</div>
 	</div>
 
+	<a href="https://github.com/initr/initr-site/issues/1" class="github">
+		Help This Project
+		<i class="icon-github"></i>
+	</a>
+
+	<footer>
+		&copy; <?= date('Y') ?> <a href="http://ryantablada.com">Ryan Tablada</a>
+	</footer>
+
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 	<srcipt src="/js/gumby.min.js"></srcipt>
 	<script>
