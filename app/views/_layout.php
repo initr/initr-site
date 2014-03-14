98d32e2aff7a93d8a29d0b1a673f5c4f87ac9eeb
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-13 23:55:28 -0500

    Framework with the whole 404 n such

---
+++ app/views/_layout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>initr</title>
+	<link rel="stylesheet" href="/css/gumby.css">
+	<script src="/js/modernizr.js"></script>
+</head>
+<body>
+	<?= $content ?>
+
+	<a href="https://github.com/initr/initr-site/issues/1" class="github">
+		Help This Project
+		<i class="icon-github"></i>
+	</a>
+
+	<footer>
+		&copy; <?= date('Y') ?> <a href="http://ryantablada.com">Ryan Tablada</a>
+	</footer>
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+	<srcipt src="/js/gumby.min.js"></srcipt>
+	<script>
+		$('.modal').on('click', '.switch', function(ev) {
+			$('.modal.active').fadeOut(function() {
+				$('.modal.active').removeClass('active');
+			});
+			return false;
+		});
+	</script>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
+function app_path($file = null) {
+	return __DIR__.'/../app' . $file;
+}
+
 $uri = $_SERVER["REQUEST_URI"];
 $uri = $uri == '/' ? 'index' : $uri;
-$filepath = __DIR__.'/../app/views/'.$uri.'.php';
+$filepath = app_path('/views/'.$uri.'.php');
 
 if (file_exists($filepath)) {
-	require($filepath);
+	require $filepath;
+} else {
+	require app_path('/views/404.php');
 }
+
+$content = ob_get_clean();
+
+include app_path('/views/_layout.php');
