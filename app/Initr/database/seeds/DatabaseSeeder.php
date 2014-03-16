be8e4eda98b79537422779beee9867eb8bfe5608
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-16 16:07:36 -0500

    First database seed and migrations for user

---
+++ app/Initr/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php namespace Initr\database\seeds;
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+
+		// $this->call('UserTableSeeder');
+	}
+
+}
