c341f5507e34b33abf35afe4798eef028066b8b7
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-17 10:51:34 -0500

    Check if valid before listing require

--- app/Initr/Applications/Manifests/views/manifests/_version-info.php
+++ app/Initr/Applications/Manifests/views/manifests/_version-info.php
@@ -4,16 +4,20 @@
 	<div class="github-info">Commit: <?= HTML::link($version->base_url, $version->sha) ?></div>
 
 	<span class="require-label">Requires:</span>
-	<ul class="requires">
-		<?php foreach($version->json_file['require'] as $requireName => $requireVersion) : ?>
-			<li><?= HTML::linkRoute('Manifests.manifests.show', $requireName, $requireName) ?>: <?= $requireVersion ?></li>
-		<?php endforeach ?>
-	</ul>
+	<?php if (isset($version->json_file['require'])) : ?>
+		<ul class="requires">
+			<?php foreach($version->json_file['require'] as $requireName => $requireVersion) : ?>
+				<li><?= HTML::linkRoute('Manifests.manifests.show', $requireName, $requireName) ?>: <?= $requireVersion ?></li>
+			<?php endforeach ?>
+		</ul>
+	<?php endif ?>
 
-	<span class="script-label">Scripts:</span>
-	<ul class="scripts">
-		<?php foreach($version->json_file['scripts'] as $script) : ?>
-			<li><?= $script ?></li>
-		<?php endforeach ?>
-	</ul>
+	<?php if (isset($version->json_file['scripts'])) : ?>
+		<span class="script-label">Scripts:</span>
+		<ul class="scripts">
+			<?php foreach($version->json_file['scripts'] as $script) : ?>
+				<li><?= $script ?></li>
+			<?php endforeach ?>
+		</ul>
+	<?php endif ?>
 </li>
