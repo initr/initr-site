b57543651c112f269c8c071444b6fcac95f8ae0e
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-16 19:10:34 -0500

    Fix routes and the 404 page

--- app/Initr/Applications/Brochure/routes.php
+++ app/Initr/Applications/Brochure/routes.php
@@ -2,16 +2,16 @@
 
 $controllerNamespace = 'Initr\\Applications\\Brochure\\Controllers';
 
-Route::group(['namespace' => $controllerNamespace, 'as' => 'Brochure'], function()
+Route::group(['namespace' => $controllerNamespace, /*'as' => 'Brochure'*/], function()
 {
-	Route::get('/', ['uses' => 'Home@index', 'as' => 'home.index']);
-	Route::get('example', ['uses' => 'Home@example', 'as' => 'home.example']);
+	Route::get('/', ['uses' => 'Home@index', 'as' => 'Brochure.home.index']);
+	Route::get('example', ['uses' => 'Home@example', 'as' => 'Brochure.home.example']);
 });
 
 
 App::missing(function($e)
 {
-	$layout = View::make('_layout');
+	$layout = View::make('Brochure::_layout');
 
 	$layout->nest('content', 'App::404');
 
--- app/Initr/Applications/Login/routes.php
+++ app/Initr/Applications/Login/routes.php
@@ -2,15 +2,15 @@
 
 $controllerNamespace = 'Initr\\Applications\\Login\\Controllers';
 
-Route::group(['namespace' => $controllerNamespace, 'as' => 'Login'], function()
+Route::group(['namespace' => $controllerNamespace, /*'as' => 'Login' */], function()
 {
-	Route::get('login', ['uses' => 'Session@create', 'as' => 'session.create']);
-	Route::post('login', ['uses' => 'Session@store', 'as' => 'session.store']);
-	Route::get('logout', ['uses' => 'Session@destroy', 'as' => 'session.destroy']);
+	Route::get('login', ['uses' => 'Session@create', 'as' => 'Login.session.create']);
+	Route::post('login', ['uses' => 'Session@store', 'as' => 'Login.session.store']);
+	Route::get('logout', ['uses' => 'Session@destroy', 'as' => 'Login.session.destroy']);
 
-	Route::get('signup', ['uses' => 'Users@create', 'as' => 'users.create']);
-	Route::post('signup', ['uses' => 'Users@store', 'as' => 'users.store']);
+	Route::get('signup', ['uses' => 'Users@create', 'as' => 'Login.users.create']);
+	Route::post('signup', ['uses' => 'Users@store', 'as' => 'Login.users.store']);
 
-	Route::get('signup/success', ['uses' => 'Users@success', 'as' => 'users.success']);
-	Route::get('signup/confirm/{token}', ['uses' => 'Users@confirm', 'as' => 'users.confirm']);
+	Route::get('signup/success', ['uses' => 'Users@success', 'as' => 'Login.users.success']);
+	Route::get('signup/confirm/{token}', ['uses' => 'Users@confirm', 'as' => 'Login.users.confirm']);
 });
