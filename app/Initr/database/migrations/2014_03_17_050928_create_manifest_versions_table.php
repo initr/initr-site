73b6d395c6c126d0dcab5220cee852ecd8102517
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-17 00:17:18 -0500

    Add version migration

---
+++ app/Initr/database/migrations/2014_03_17_050928_create_manifest_versions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManifestVersionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('manifest_versions', function($table) {
+			$table->increments('id');
+			$table->integer('manifest_url');
+			$table->string('version_name');
+			$table->text('json_file');
+			$table->string('base_url');
+			$table->text('compiled_scripts');
+			$table->text('compiled_response');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('manifest_versions');
+	}
+
+}
