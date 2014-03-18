ea73338fcc67585c2d6261861ad9e4c56c973126
Ryan Tablada <ryan@Ryans-MacBook-Pro.local>
2014-03-17 20:34:02 -0500

    Search form

--- app/Initr/Applications/Manifests/views/manifests/index.php
+++ app/Initr/Applications/Manifests/views/manifests/index.php
@@ -2,6 +2,18 @@
 	<div class="manifest-info">
 		<div class="main-info">
 			<h2>Search Manifests</h2>
+
+			<?= Form::open(['method' => 'GET']) ?>
+				<div class="field">
+					<?= Form::label('q', 'Search:') ?>
+					<?= Form::text('q', Input::get('q'), ['class' => 'input']) ?>
+				</div>
+				<div class="row">
+					<div class="btn medium primary search">
+						<?= Form::submit('Search') ?>
+					</div>
+				</div>
+			<?= Form::close() ?>
 		</div>
 
 		<ul class="manifests">
